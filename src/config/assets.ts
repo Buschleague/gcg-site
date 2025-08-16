0e70402ef3cdace9b49cd22e38b892f5acd626e4
Buschleague <david@bpsaisoftware.com>
2025-08-16 13:40:53 -0500

    Replace `handcrafted.pdf` with `handcrafted.png` in assets configuration and update static assets folder accordingly.

--- src/config/assets.ts
+++ src/config/assets.ts
@@ -1,6 +1,6 @@
 export const assetPaths = {
   hero: './assets/GeminiCG.png',
-  handcrafted: './assets/handcrafted.pdf',
+  handcrafted: './assets/handcrafted.png',
   ozark: './assets/ozark.jpg',
   sparkle: ['./assets/sparkle-logo.jpeg', './assets/sparkle-logo.jpg']
 } as const;
