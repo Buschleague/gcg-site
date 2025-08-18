d0a75091a813297f58c8d490b2081b798ba386cf
Buschleague <david@bpsaisoftware.com>
2025-08-18 14:16:30 -0500

    Remove alternate `sparkle-logo.jpg` from assets configuration

--- src/config/assets.ts
+++ src/config/assets.ts
@@ -2,5 +2,5 @@ export const assetPaths = {
   hero: './assets/GeminiCG.png',
   handcrafted: './assets/handcrafted.png',
   ozark: './assets/ozark.jpg',
-  sparkle: ['./assets/sparkle-logo.jpeg', './assets/sparkle-logo.jpg']
+  sparkle: './assets/sparkle-logo.jpeg',
 } as const;
